2dcca14c5e012a7a6e951f7f5be4c1931c75ae37
Vo Quang Chien <voquangchien.dev@proton.me>
2024-10-27 09:46:14 +0700

    add rofi config

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@
 sudo pacman -Syu
 
 # Install and setup Hyprland
-sudo pacman -S hyprland hyprpaper --needed
+sudo pacman -S hyprland hyprpaper hyprlock --needed
 cp ./hypr/ ~/.config/
 
 # Install and setup Neovim
 sudo pacman -S neovim
 cp -r ./nvim/ ~/.config/
+
+# Install and setup rofi
+sudo pacman -S rofi-wayland
+cp -r ./rofi/ ~/.config/
