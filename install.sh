fc395272b82e03539525973169cacbd8543826f3
Vo Quang Chien <voquangchien.dev@proton.me>
2024-10-27 11:29:02 +0700

    add waybar

--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
-# Update system
-sudo pacman -Syu
+bootstrap() {
+  # Update system
+  sudo pacman -Syu
 
-# Install and setup Hyprland
-sudo pacman -S hyprland hyprpaper hyprlock --needed
-cp ./hypr/ ~/.config/
+  # Install and setup Hyprland
+  sudo pacman -S hyprland hyprpaper hyprlock --needed
+  cp ./hypr/ ~/.config/
 
-# Install and setup Neovim
-sudo pacman -S neovim
-cp -r ./nvim/ ~/.config/
+  # Install and setup Neovim
+  sudo pacman -S neovim
+  cp -r ./nvim/ ~/.config/
 
-# Install and setup rofi
-sudo pacman -S rofi-wayland
-cp -r ./rofi/ ~/.config/
+  # Install and setup rofi
+  sudo pacman -S rofi-wayland
+  cp -r ./rofi/ ~/.config/
 
-# Setup Bash
-cp ./.bashrc ~/
+  # Setup Bash
+  cp ./.bashrc ~/
 
-# Install and setup starship
-sudo pacman -S starship
-cp ./starship.toml ~/.config/
+  # Install and setup starship
+  sudo pacman -S starship
+  cp ./starship.toml ~/.config/
+
+  # Install and setup waybar
+  sudo pacman -S waybar
+  cp ./waybar/ ~/.config/
+}
+
+case "$1" in
+"--bootstrap")
+  bootstrap
+  ;;
+esac
