46a407b39d007c0a1df06e5585b7cde1b8e3c796
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-09 16:20:26 +0700

    update

--- hypr/scripts/random_background.sh
+++ hypr/scripts/random_background.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+DIR="$HOME/Pictures/backgrounds/"
+
 hyprpaper &
 
 sleep 1
 
-hyprctl hyprpaper wallpaper "eDP-1,$(hyprctl hyprpaper listloaded | shuf -n1)"
+if [ -d "$DIR" ]; then
+  random_background=$(find "$DIR" -name "bg*" | shuf -n1)
+  hyprctl hyprpaper unload all
+  hyprctl hyprpaper preload "$random_background"
+  hyprctl hyprpaper wallpaper "eDP-1, $random_background"
+fi
