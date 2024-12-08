78bce7b55188972c1872538d0c6a09f4142edaf5
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-08 11:13:31 +0700

    fix random background

--- .config/hypr/scripts/random_background.sh
+++ .config/hypr/scripts/random_background.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 DIR="$HOME/backgrounds/"
+LAST_BG_FILE="/tmp/last_background"
 
 if [ ! $(pgrep -x hyprpaper) ]; then
-	hyprpaper &
-	sleep 1
+  hyprpaper &
+  sleep 1
 fi
 
 if [ -d "$DIR" ]; then
-	notify-send "Background changed"
-	random_background=$(find "$DIR" -name "bg*" | shuf -n1)
-	hyprctl hyprpaper unload all
-	hyprctl hyprpaper preload "$random_background"
-	hyprctl hyprpaper wallpaper "eDP-1, $random_background"
+  while true; do
+    random_background=$(find "$DIR" -name "bg*" | shuf -n1)
+    [[ ! -f "$LAST_BG_FILE" || "$random_background" != "$(cat "$LAST_BG_FILE")" ]] && break
+  done
+  hyprctl hyprpaper unload all
+  hyprctl hyprpaper preload "$random_background"
+  hyprctl hyprpaper wallpaper "eDP-1, $random_background"
+  echo "$random_background" >"$LAST_BG_FILE"
+  notify-send "Background changed"
 fi
