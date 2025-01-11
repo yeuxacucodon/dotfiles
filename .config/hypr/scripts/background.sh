271b665cf79df469edd8919e56b69f6e4c942e49
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-11 23:15:14 +0700

    update

--- .config/hypr/scripts/background.sh
+++ .config/hypr/scripts/background.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
+if [[ ! $(pgrep -x swww-daemon) ]]; then
+  swww-daemon
+fi
+
 DIR="$HOME/backgrounds/"
 LAST_BG_FILE="/tmp/last_background"
 
-if [ ! $(pgrep -x hyprpaper) ]; then
-  hyprpaper &
-  sleep 1
-fi
-
 if [ -d "$DIR" ]; then
   while true; do
-    random_background=$(find "$DIR" -name "bg*" | shuf -n1)
+    random_background=$(find "$DIR" -type f | shuf -n1)
     [[ ! -f "$LAST_BG_FILE" || "$random_background" != "$(cat "$LAST_BG_FILE")" ]] && break
   done
-  hyprctl hyprpaper unload all
-  hyprctl hyprpaper preload "$random_background"
-  hyprctl hyprpaper wallpaper "eDP-1, $random_background"
+
+  swww img --transition-duration 1 --transition-step 20 --transition-type wipe "$random_background"
   echo "$random_background" >"$LAST_BG_FILE"
   notify-send "Background changed"
 fi
